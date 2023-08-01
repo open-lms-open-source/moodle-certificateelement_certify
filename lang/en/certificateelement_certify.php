80697848415ee09fbd4d6cce1cf791fc5da0348f
Petr Skoda <petr.skoda@openlms.net>
2023-08-01 10:02:40 +0200

    OWORK-654 add tool_certify

---
+++ lang/en/certificateelement_certify.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of the tool_certificate plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'certificateelement_certify', language 'en'.
+ *
+ * @package    certificateelement_certify
+ * @copyright  2023 Open LMS (https://www.openlms.net/)
+ * @author     Petr Skoda
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Certification field';
+$string['privacy:metadata'] = 'The certification fields plugin does not store any personal data.';
+$string['certificationfield'] = 'Certification field';
+$string['certificationfield_help'] = 'This is the certification field that will be displayed on the PDF.';
